2671331b74bdc51ab012fbbb50ee86b9fac5290a
Pham Viet Hai <pvhmain2k3@gmail.com>
2024-05-29 17:44:37 +0700

    middleware101

--- Backend/3.4 Middleware/index3.js
+++ Backend/3.4 Middleware/index3.js
@@ -3,6 +3,11 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+function logger(req, res, next){
+  console.log(`url: ${req.url}`);
+  next();
+}
+
 app.use(logger);
 
 app.get("/", (req, res) => {
